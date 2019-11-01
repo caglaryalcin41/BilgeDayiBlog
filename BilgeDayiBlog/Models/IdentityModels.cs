f47d40d0c4f8f8298fe650d143d06f076210c84d
caglar.yalcin <caglar.yalcin41@gmail.com>
2019-11-01 21:24:50 +0300

    model hazır

--- BilgeDayiBlog/Models/IdentityModels.cs
+++ BilgeDayiBlog/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,6 +17,8 @@ namespace BilgeDayiBlog.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public virtual ICollection<Post> Posts { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -29,5 +32,9 @@ namespace BilgeDayiBlog.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
     }
 }
---
+++ BilgeDayiBlog/Models/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BilgeDayiBlog.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Author")]
+        [Display(Name = "Yazar")]
+        public string AuthorId { get; set; }
+
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name ="Başlık")]
+        public string Title { get; set; }
+
+        [Display(Name = "İçerik")]
+        public string  Content { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+
+        [Required]
+        public DateTime? CreationTime { get; set; }
+
+        public virtual Category category { get; set; }
+    }
+}
