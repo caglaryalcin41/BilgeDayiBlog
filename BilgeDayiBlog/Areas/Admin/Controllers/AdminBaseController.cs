ecbb4f76ac47a9360c060277a1705859029f070b
caglar.yalcin <caglar.yalcin41@gmail.com>
2019-11-05 21:31:22 +0300

    Admin layout düzenlemeler ve türkçeleştime yapıldı

---
+++ BilgeDayiBlog/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,27 @@
+﻿using BilgeDayiBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BilgeDayiBlog.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public abstract class AdminBaseController : Controller
+    {
+        // GET: Admin/AdminBase
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
--- BilgeDayiBlog/Areas/Admin/Controllers/PostsController.cs
+++ BilgeDayiBlog/Areas/Admin/Controllers/PostsController.cs
@@ -6,10 +6,9 @@ using System.Web.Mvc;
 
 namespace BilgeDayiBlog.Areas.Admin.Controllers
 {
-    
-    public class DashboardController : AdminBaseController
+    public class PostsController : Controller
     {
-        // GET: Admin/Dashboard
+        // GET: Admin/Posts
         public ActionResult Index()
         {
             return View();
